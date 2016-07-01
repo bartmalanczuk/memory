3d4482fcc19424107f316fddf3a9aa6bb2824043
Bart <bart.malanczuk@gmail.com>
2016-07-02 01:53:49 +0200

    Initialize boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream'),
+    concat = require('gulp-concat');
+
+var config = {
+  paths: {
+    html: './src/*.html',
+    js: './src/scripts/**/*.js',
+    images: './src/images/*',
+    css: [
+      'src/styles/main.css',
+      'node_modules/foundation-sites/dist/foundation.css'
+    ],
+    mainJs: './src/scripts/main.js',
+    dist: './dist'
+  }
+};
+
+gulp.task('html', function() {
+  gulp.src(config.paths.html)
+    .pipe(gulp.dest(config.paths.dist))
+});
+
+gulp.task('js', function() {
+  browserify(config.paths.mainJs)
+    .transform("babelify", {presets: ["es2015", "react"]})
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+});
+
+gulp.task('css', function() {
+  gulp.src(config.paths.css)
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest(config.paths.dist + '/styles'))
+});
+
+gulp.task('images', function() {
+  gulp.src(config.paths.images)
+    .pipe(gulp.dest(config.paths.dist + '/images'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(config.paths.html, ['html'])
+  gulp.watch(config.paths.js, ['js'])
+});
+
+gulp.task('default', ['html', 'js', 'css', 'images', 'watch']);
